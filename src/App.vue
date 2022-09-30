477a10f1e2f2994b5c199d048a8dd29defc4b7d0
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-09-30 16:08:32 -0300

    main logic and initial style

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+let currentTurn = ref<string>("X");
+let square = ref<string[]>([]);
+let gameOver = ref<boolean>(false);
+
+for (let i = 0; i <= 9; i++) {
+  square.value.push("empty");
+}
+square.value[0] = "nothing";
+
+const clickedSquare = (n: number) => {
+  if (square.value[n] !== "empty") return;
+  square.value[n] = currentTurn.value;
+  isWinner();
+  isCat();
+  currentTurn.value === "X"
+    ? (currentTurn.value = "O")
+    : (currentTurn.value = "X");
+  console.log(currentTurn.value);
+};
+
+const isWinner = () => {
+  const lines = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      square.value[a] !== "empty" &&
+      square.value[a] === square.value[b] &&
+      square.value[a] === square.value[c]
+    ) {
+      gameOver.value = true;
+    }
+  }
+};
+
+const isCat = () => {
+  let isCats = true;
+  square.value.forEach((item) => {
+    if (item === "empty") {
+      isCats = false;
+    }
+  });
+  if (isCats === true) {
+    gameOver.value = true;
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div v-if="!gameOver" id="board">
+    <div class="square" v-for="n in 9" :key="n" @click="clickedSquare(n)">
+      {{ square[n] }}
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+#board {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 400px;
+  height: 400px;
+  margin: 0 auto;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.square {
+  width: 30%;
+  border: 2px solid #000;
+  color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
